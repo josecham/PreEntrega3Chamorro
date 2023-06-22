adff0fac723e1970c6529357f18980854d7e11f9
josecham <gabriel_influencia74@hotmail.com>
2023-06-22 11:09:35 -0300

    la etiqueta de lo que guardo

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -2,10 +2,10 @@
 let condition = true
 let años = Number(prompt('ingrese año de nacimento: '))
 while (condition) {
-if (años <= 2005){
-alert("Bienvenido")
+if (años < 2005){
+alert("Bienvenido es mayor de 18")
 condition = false
-} else{
+} else {
 alert ("no puede ver la pagina")
 años = Number(prompt('ingrese año de nacimiento: '))
 
@@ -44,55 +44,66 @@ function Persona (nombre, apellido, email, nft){
     const apellido = prompt ("ingresar apellido") 
     const email = prompt ("ingresar email")
     const nft = prompt ("numero de NFT que deseo comprar del 1 al 12")
-    
+
     const persona = new Persona(nombre, apellido, email, nft)
     console.log(persona)
     // const persona1 = new Persona(nombre, apellido, email, nft)
     //console.log(persona1)
 
-//OBJETOS Simulador de Iva
-class Producto {
-    constructor(nombre, precio) {
-        this.nombre  = nombre.toUpperCase();
-        this.precio  = parseFloat(precio);
-        this.vendido = false;
-    }
-    sumaIva() {
-        this.precio = this.precio * 1.21;
-    }
-    vender() {
-        this.vendido = true;
-    }
-}
-const producto1 = new Producto("nft1", "2000");
-const producto2 = new Producto("nft2", "2000");
-const producto3 = new Producto("nft3", "2000");
-const producto4 = new Producto("nft4", "2000");
-const producto5 = new Producto("nft5", "2000");
-const producto6 = new Producto("nft6", "2000");
-const producto7 = new Producto("nft7", "2000");
-const producto8 = new Producto("nft8", "2000");
-const producto9 = new Producto("nft9", "2000");
-const producto10 = new Producto("nft10", "2000");
-const producto11= new Producto("nft11", "2000");
-const producto12= new Producto("nft12", "2000");
-producto1.sumaIva();
-producto2.sumaIva();
-producto3.sumaIva();
-producto4.sumaIva();
-producto5.sumaIva();
-producto6.sumaIva();
-producto7.sumaIva();
-producto8.sumaIva();
-producto9.sumaIva();
-producto10.sumaIva();
-producto11.sumaIva();
-producto12.sumaIva();
-producto1.vender();
-producto5.vender();
-producto8.vender();
-producto12.vender();
+    //OBJETO y ARRAY
 
-console.log(producto1);
+    class Producto {
+        constructor(nombre, precio) {
+            this.nombre  = nombre.toUpperCase();
+            this.precio  = parseFloat(precio);
+            this.vendido = false;
+        }
+        sumaIva() {
+            this.precio = this.precio * 1.21;
+        }
+        vender() {
+            this.vendido = true;
+        }
+    }const productos = [];
+    productos.push(new Producto("nft1", "2000"));
+    productos.push(new Producto("nft2", "2000"));
+    productos.push(new Producto("nft3", "2000"));
+    productos.push(new Producto("nft4", "2000"));
+    productos.push(new Producto("nft5", "2000"));
+    productos.push(new Producto("nft6", "2000"));
+    productos.push(new Producto("nft7", "2000"));
+    productos.push(new Producto("nft8", "2000"));
+    productos.push(new Producto("nft9", "2000"));
+    productos.push(new Producto("nft10", "2000"));
+    productos.push(new Producto("nft11", "2000"));
+    productos.push(new Producto("nft12", "2000"));
 
-alert ("precio final: " + producto1.precio)
+    //para sumarle el iva
+    for (const producto of productos)
+        producto.sumaIva();
+    
+    console.log(productos)
+ 
+    //Agregue la funcion vender para que me modifique el producto vendido a true
+    //for (const producto of productos)
+    //producto.vender();
+    //console.log(productos)
+
+    // Funcion quedan() para saber cuantos Nft tengo disponibles
+    
+    let nftdisponibles = 11
+    function quedan(){ 
+    console.log("quedan: "+nftdisponibles)
+    alert("NFT DISPONIBLES: "+nftdisponibles);
+    nftdisponibles = nftdisponibles -1; 
+    return nftdisponibles
+    }
+    quedan()
+
+    function saludar() {
+             
+            alert("Muchas Gracias!! solo resta completar el formulario para efectuar la compra")
+        
+        }
+        
+        saludar()   
