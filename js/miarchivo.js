4341e1bbc8fd57a8cf482bea0c4345f10d6c5ce2
josecham <gabriel_influencia74@hotmail.com>
2023-07-03 22:12:22 -0300

    la etiqueta de lo que guardo

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,13 +1,19 @@
 //constructor Date dias que se mantiene la oferta redondeado hacia abajo con Math.floor
-const navidad = new Date("July 14, 2023")
+const promocion = new Date("July 14, 2023")
 const hoy = (new Date())
 
 const milisegundosPorDia = 86400000
 
-console.log( Math.floor((navidad - hoy ) / milisegundosPorDia) )
+console.log( Math.floor((promocion - hoy ) / milisegundosPorDia) )
 
-alert("Oferta Valida por (dias)= " +  Math.floor((navidad - hoy ) / milisegundosPorDia) )
+alert("Promoción Valida por " +  Math.floor((promocion - hoy ) / milisegundosPorDia) + "dias" )
 
+//los dias que restan de la promocion visualizado en la pagina con queryselector 
+let promocion1 = document.querySelector("#promo");
+promocion1.innerHTML = "Promoción Valida por " +  Math.floor((promocion - hoy ) / milisegundosPorDia) + "dias";
+promocion1.style.background = "lightgreen";
+promocion1.style.color = "black";
+promocion1.style.borderRadius = "4px";
 
 //algoritmo con condicional if else
 let condition = true
@@ -42,7 +48,7 @@ while(( entrada !== "SI" ) && (entrada !== "si"))
    entrada = prompt("¿desea comprar un NFT?SI/NO");
    
 }
-//FUNCION Constructoras para completar los datos del usuario (Pascalcase string)
+//FUNCION Constructor para completar los datos del usuario (Pascalcase string)
 function Persona (nombre, apellido, email, nft){ 
     this.nombre = nombre 
     this.apellido = apellido 
@@ -118,6 +124,12 @@ function Persona (nombre, apellido, email, nft){
     alert("PRECIO FINAL + IVA= "+ buscado.precio * 1.21);
     alert("STOCK= "+ buscado.vendido.toLocaleUpperCase());
     
+    //PRECIO TOTAL EN EL CARRITO visualizado en la pagina con queryselector dependiendo del precio del nft
+    let Total = document.querySelector("#Total");
+    Total.innerHTML = "TOTAL $" + buscado.precio * 1.21;
+    Total.style.background = "lightgreen";
+    Total.style.color = "black";
+    Total.style.borderRadius = "4px";
 
     // Funcion quedan() para saber cuantos Nft tengo disponibles
     let nftdisponibles = 11
@@ -136,3 +148,201 @@ function Persona (nombre, apellido, email, nft){
     }
         
     saludar()   
+
+    //DOOM usando id
+    let elementoPorId1 = document.getElementById("precio1");
+    elementoPorId1.innerHTML = "$ 2500";
+    let elementos = document.getElementById("stock1");
+    elementos.innerHTML = "Disponible";
+    elementos.style.background = "lightgreen";
+    elementos.style.color = "black";
+    elementos.style.borderRadius = "4px";
+
+    let elementoPorId2 = document.getElementById("precio2");
+    elementoPorId2.innerHTML = "$ 2000";
+    let elementos2 = document.getElementById("stock2");
+    elementos2.innerHTML = "Disponible";
+    elementos2.style.background = "lightgreen";
+    elementos2.style.color = "black";
+    elementos2.style.borderRadius = "4px";
+
+    let elementoPorId3 = document.getElementById("precio3");
+    elementoPorId3.innerHTML = "$ 2000";
+    let elementos3 = document.getElementById("stock3");
+    elementos3.innerHTML = "Vendido";
+    elementos3.style.background = "#ffcccb";
+    elementos3.style.color = "black";
+    elementos3.style.borderRadius = "4px";
+
+    let elementoPorId4 = document.getElementById("precio4");
+    elementoPorId4.innerHTML = "$ 2000";
+    let elementos4 = document.getElementById("stock4");
+    elementos4.innerHTML = "Disponible";
+    elementos4.style.background = "lightgreen";
+    elementos4.style.color = "black";
+    elementos4.style.borderRadius = "4px";
+
+    let elementoPorId5 = document.getElementById("precio5");
+    elementoPorId5.innerHTML = "$ 2100";
+    let elementos5 = document.getElementById("stock5");
+    elementos5.innerHTML = "Disponible";
+    elementos5.style.background = "lightgreen";
+    elementos5.style.color = "black";
+    elementos5.style.borderRadius = "4px";
+
+    let elementoPorId6 = document.getElementById("precio6");
+    elementoPorId6.innerHTML = "$ 2100";
+    let elementos6 = document.getElementById("stock6");
+    elementos6.innerHTML = "Disponible";
+    elementos6.style.background = "lightgreen";
+    elementos6.style.color = "black";
+    elementos6.style.borderRadius = "4px";
+
+    let elementoPorId7 = document.getElementById("precio7");
+    elementoPorId7.innerHTML = "$ 2100";
+    let elementos7 = document.getElementById("stock7");
+    elementos7.innerHTML = "Disponible";
+    elementos7.style.background = "lightgreen";
+    elementos7.style.color = "black";
+    elementos7.style.borderRadius = "4px";
+
+    let elementoPorId8 = document.getElementById("precio8");
+    elementoPorId8.innerHTML = "$ 2200";
+    let elementos8 = document.getElementById("stock8");
+    elementos8.innerHTML = "Disponible";
+    elementos8.style.background = "lightgreen";
+    elementos8.style.color = "black";
+    elementos8.style.borderRadius = "4px";
+
+    let elementoPorId9 = document.getElementById("precio9");
+    elementoPorId9.innerHTML = "$ 2200";
+    let elementos9 = document.getElementById("stock9");
+    elementos9.innerHTML = "Disponible";
+    elementos9.style.background = "lightgreen";
+    elementos9.style.color = "black";
+    elementos9.style.borderRadius = "4px";
+
+    let elementoPorId10 = document.getElementById("precio10");
+    elementoPorId10.innerHTML = "$ 2200";
+    let elementos10 = document.getElementById("stock10");
+    elementos10.innerHTML = "Disponible";
+    elementos10.style.background = "lightgreen";
+    elementos10.style.color = "black";
+    elementos10.style.borderRadius = "4px";
+
+    let elementoPorId11 = document.getElementById("precio11");
+    elementoPorId11.innerHTML = "$ 2300";
+    let elementos11 = document.getElementById("stock11");
+    elementos11.innerHTML = "Disponible";
+    elementos11.style.background = "lightgreen";
+    elementos11.style.color = "black";
+    elementos11.style.borderRadius = "4px";
+
+    let elementoPorId12 = document.getElementById("precio12");
+    elementoPorId12.innerHTML = "$ 2300";
+    let elementos12 = document.getElementById("stock12");
+    elementos12.innerHTML = "Disponible";
+    elementos12.style.background = "lightgreen";
+    elementos12.style.color = "black";
+    elementos12.style.borderRadius = "4px";
+
+    //MODIFICAR NODO
+    let modificado = document.querySelector ("#precio1");
+    modificado.textContent = "$ 3025";
+
+    //DOOM carrito usando querySelector
+    let elementoPorId1c = document.querySelector("#precio1c");
+    elementoPorId1c.innerHTML = "$ 2500";
+    let elementosc = document.querySelector("#stock1c");
+    elementosc.innerHTML = "Disponible";
+    elementosc.style.background = "lightgreen";
+    elementosc.style.color = "black";
+    elementosc.style.borderRadius = "4px";
+
+    let elementoPorId2c = document.querySelector("#precio2c");
+    elementoPorId2c.innerHTML = "$ 2000";
+    let elementos2c = document.querySelector("#stock2c");
+    elementos2c.innerHTML = "Disponible";
+    elementos2c.style.background = "lightgreen";
+    elementos2c.style.color = "black";
+    elementos2c.style.borderRadius = "4px";
+
+    let elementoPorId3c = document.querySelector("#precio3c");
+    elementoPorId3c.innerHTML = "$ 2000";
+    let elementos3c = document.querySelector("#stock3c");
+    elementos3c.innerHTML = "Vendido";
+    elementos3c.style.background = "#ffcccb";
+    elementos3c.style.color = "black";
+    elementos3c.style.borderRadius = "4px";
+
+    let elementoPorId4c = document.querySelector("#precio4c");
+    elementoPorId4c.innerHTML = "$ 2000";
+    let elementos4c = document.querySelector("#stock4c");
+    elementos4c.innerHTML = "Disponible";
+    elementos4c.style.background = "lightgreen";
+    elementos4c.style.color = "black";
+    elementos4c.style.borderRadius = "4px";
+
+    let elementoPorId5c = document.querySelector("#precio5c");
+    elementoPorId5c.innerHTML = "$ 2100";
+    let elementos5c = document.querySelector("#stock5c");
+    elementos5c.innerHTML = "Disponible";
+    elementos5c.style.background = "lightgreen";
+    elementos5c.style.color = "black";
+    elementos5c.style.borderRadius = "4px";
+
+    let elementoPorId6c = document.querySelector("#precio6c");
+    elementoPorId6c.innerHTML = "$ 2100";
+    let elementos6c = document.querySelector("#stock6c");
+    elementos6c.innerHTML = "Disponible";
+    elementos6c.style.background = "lightgreen";
+    elementos6c.style.color = "black";
+    elementos6c.style.borderRadius = "4px";
+
+    let elementoPorId7c = document.querySelector("#precio7c");
+    elementoPorId7c.innerHTML = "$ 2100";
+    let elementos7c = document.querySelector("#stock7c");
+    elementos7c.innerHTML = "Disponible";
+    elementos7c.style.background = "lightgreen";
+    elementos7c.style.color = "black";
+    elementos7c.style.borderRadius = "4px";
+
+    let elementoPorId8c = document.querySelector("#precio8c");
+    elementoPorId8c.innerHTML = "$ 2200";
+    let elementos8c = document.querySelector("#stock8c");
+    elementos8c.innerHTML = "Disponible";
+    elementos8c.style.background = "lightgreen";
+    elementos8c.style.color = "black";
+    elementos8c.style.borderRadius = "4px";
+
+    let elementoPorId9c = document.querySelector("#precio9c");
+    elementoPorId9c.innerHTML = "$ 2200";
+    let elementos9c = document.querySelector("#stock9c");
+    elementos9c.innerHTML = "Disponible";
+    elementos9c.style.background = "lightgreen";
+    elementos9c.style.color = "black";
+    elementos9c.style.borderRadius = "4px";
+
+    let elementoPorId10c = document.querySelector("#precio10c");
+    elementoPorId10c.innerHTML = "$ 2200";
+    let elementos10c = document.querySelector("#stock10c");
+    elementos10c.innerHTML = "Disponible";
+    elementos10c.style.background = "lightgreen";
+    elementos10c.style.color = "black";
+    elementos10c.style.borderRadius = "4px";
+
+    let elementoPorId11c = document.querySelector("#precio11c");
+    elementoPorId11c.innerHTML = "$ 2300";
+    let elementos11c = document.querySelector("#stock11c");
+    elementos11c.innerHTML = "Disponible";
+    elementos11c.style.background = "lightgreen";
+    elementos11c.style.color = "black";
+    elementos11c.style.borderRadius = "4px";
+
+    let elementoPorId12c = document.querySelector("#precio12c");
+    elementoPorId12c.innerHTML = "$ 2300";
+    let elementos12c = document.querySelector("#stock12c");
+    elementos12c.innerHTML = "Disponible";
+    elementos12c.style.background = "lightgreen";
+    elementos12c.style.color = "black";
+    elementos12c.style.borderRadius = "4px";
